54d4417a4102a3bc9008b533a0133b71b7f44f9b
Hugo Ribeira <hugoribeira@gmail.com>
2017-09-30 23:33:51 +0100

    Initial commit

---
+++ lib/ex_job_benchmark.ex
@@ -0,0 +1,18 @@
+defmodule ExJobBenchmark do
+  @moduledoc """
+  Documentation for ExJobBenchmark.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExJobBenchmark.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_job_benchmark_test.exs
@@ -0,0 +1,8 @@
+defmodule ExJobBenchmarkTest do
+  use ExUnit.Case
+  doctest ExJobBenchmark
+
+  test "greets the world" do
+    assert ExJobBenchmark.hello() == :world
+  end
+end
